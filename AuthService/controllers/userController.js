7fc86bfacb9559cc6e0cec728e7f2afdcc31804e
Joseph <tranduykhuongit@gmail.com>
2023-12-28 21:36:02 +0700

    fix status code

--- AuthService/controllers/userController.js
+++ AuthService/controllers/userController.js
@@ -5,7 +5,7 @@ const prisma = require('../prisma/prisma');
 exports.getUserByZaloId = catchAsync(async (req, res, next) => {
     const zaloId = req?.params?.id || ''
 
-    const user = await prisma.user.findFirst({
+    const user = await prisma.user.findFirstOrThrow({
         where: {
             zalo_id: zaloId
         },
@@ -19,10 +19,6 @@ exports.getUserByZaloId = catchAsync(async (req, res, next) => {
             }
         }
     })
-
-    if (!user) {
-        return next(new AppError("Not found", 404))
-    }
 
     res.status(200).json(user);
 })
